4498f5f4fe92b29a2008502877ea2a9bd39d575f
Tri Asep Tumbara <triaseptmbr@gmail.com>
2025-10-31 18:23:08 +0700

    Add display sorted character number

--- main.py
+++ main.py
@@ -1,15 +1,28 @@
 from stats import get_total_words
+from stats import get_total_characters
+from stats import get_total_characters_sorted
 
 def get_book_text(path):
     with open(path) as file:
-        file_contents = file.read()
+        file_contents = file.read().replace('\n', ' ')
 
         return file_contents
 
 def main():
     content_path = "books/frankenstein.txt"
-    num_words = get_total_words(get_book_text(content_path).replace('\n', ' '))
+    num_words = get_total_words(get_book_text(content_path))
+    num_characters = get_total_characters(get_book_text(content_path))
+    sorted_characters = get_total_characters_sorted(num_characters)
 
+    print("============ BOOKBOT ============")
+    print(f"Analyzingbook found at {content_path}...")
+    print("----------- Word Count ----------")
     print(f"Found {num_words} total words")
+    print("--------- Character Count -------")
+
+    for character in sorted_characters:
+        print(f"{character["name"]}: {character["num"]}")
+
+    print("============= END ===============")
 
 main()
--- stats.py
+++ stats.py
@@ -2,3 +2,37 @@ def get_total_words(content):
     total_words = len(content.split())
 
     return total_words
+
+def get_total_characters(content):
+    total_chars = {}
+
+    for char in content:
+        char = char.lower()
+
+        if char in total_chars:
+            total_chars[char] = total_chars[char] + 1
+        else:
+            total_chars[char] = 1
+
+    return total_chars
+
+def get_total_characters_sorted(content_dict):
+    array_of_characters = []
+
+    for item in content_dict:
+        dict_of_characters = {}
+
+        if not item.isalpha():
+            continue
+
+        dict_of_characters["name"] = item
+        dict_of_characters["num"] = content_dict[item]
+
+        array_of_characters.append(dict_of_characters)
+    
+    def sort_item(item):
+        return item["num"]
+
+    array_of_characters.sort(reverse=True, key=sort_item)
+
+    return array_of_characters
